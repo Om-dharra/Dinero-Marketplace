23bdf0b644445d42e05538c037110f77e4fad5f1
Om-dharra <omdharra4104@gmail.com>
2025-06-09 12:50:58 +0530

    Deployment fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,8 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function generateTenantURL(tenantSlug: string) {
-  
-  if(process.env.NODE_ENV=== "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_SUBDOMAIN_ROUTING!);
+
+  if(isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
